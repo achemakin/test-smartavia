1444e924fbd46e2714c53652a6fd5db9b5fd5908
achemakin <kotor79@gmail.com>
2022-11-24 22:01:16 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import useStore from './store';
+// import Header from './layout/Header';
+// import Main from './layout/Main';
+
+useStore.getState();
+
+const App: FC = () => {
+  return (
+    <>
+      {/* <Header />
+      <Main /> */}
+    </>
+  );
+};
+
+export default App;
